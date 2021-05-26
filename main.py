764055fc23ba789963b3c42153f8e896e2b0a080
Sasuke99 <linh.huynh.dev.99@gmail.com>
2021-05-26 15:47:29 +0700

    - def gánh hoàn thành - def vây chưa đúng

--- main.py
+++ main.py
@@ -25,19 +25,57 @@ def board_clone(board):
 
 
 def updateBoard(board, start, end):
+    x1, y1 = start
+    x2, y2 = end
+    board[x2][y2] = board[x1][y1]
+    board[x1][y1] = 0
+    board = ganh(board, (x2, y2))
     return board
 
 
 def ganh(board, current):
+    player = board[current[0]][current[1]]
     # check row
-
+    if current[1] > 0 and current[1] < 4:
+        enemy1 = board[current[0]][current[1]-1]
+        enemy2 = board[current[0]][current[1]+1]
+        if enemy1 == player*(-1) and enemy2 == player*(-1):
+            board[current[0]][current[1]-1] = player
+            board[current[0]][current[1]+1] = player
     # check col
+    if current[0] > 0 and current[0] < 4:
+        enemy1 = board[current[0]-1][current[1]]
+        enemy2 = board[current[0]+1][current[1]]
+        if enemy1 == player*(-1) and enemy2 == player*(-1):
+            board[current[0]-1][current[1]] = player
+            board[current[0]+1][current[1]] = player
 
     # check cross line
+    if (current[0]+current[1]) % 2 == 0 and current[0] > 0 and current[0] < 4 and current[1] > 0 and current[1] < 4:
+        enemy1 = board[current[0]-1][current[1]-1]
+        enemy2 = board[current[0]+1][current[1]+1]
+        if enemy1 == player*(-1) and enemy2 == player*(-1):
+            board[current[0]-1][current[1]-1] = player
+            board[current[0]+1][current[1]+1] = player
+        enemy1 = board[current[0]-1][current[1]+1]
+        enemy2 = board[current[0]+1][current[1]-1]
+        if enemy1 == player*(-1) and enemy2 == player*(-1):
+            board[current[0]-1][current[1]+1] = player
+            board[current[0]+1][current[1]-1] = player
+
     return board
 
 
-def vay(board):
+def vay(board, player):
+    # vay này chưa đúng
+    # enemy = -1 * player
+    # for i in range(0, 5):
+    #     for j in range(0, 5):
+    #         if board[i][j] == enemy:
+    #             hints = moveHint(board, (i, j))
+    #             print(hints)
+    #             if hints == []:
+    #                 board[i][j] = player
     return board
 
 
@@ -135,25 +173,42 @@ def move(board, player):
     maxPoint = 0
     start = None
     end = None
+    savePoint = []
     for i in range(0, 5):
         for j in range(0, 5):
             if board[i][j] == player:
                 hints = moveHint(board, (i, j))
                 for hint in hints:
-                    print(hint)
-                    # if hint[0] > maxPoint:
-                    #     maxPoint = hint[0]
-                    #     start = hint[1]
-                    #     end = hint[2]
-    if start == None:
+                    # print(hint[0])
+                    if hint[0] == maxPoint:
+                        savePoint.append(hint)
+                        maxPoint = hint[0]
+                    if hint[0] > maxPoint:
+                        savePoint.clear()
+                        savePoint.append(hint)
+                        maxPoint = hint[0]
+    # print("==================")
+    if savePoint == []:
         return None
     else:
+        idx = random.randint(0, len(savePoint)-1)
+        hint = savePoint[idx]
+        start = hint[1]
+        end = hint[2]
         return (start, end)
+
+
+def printBoard(board):
+    for i in range(0, 5):
+        print("%d\t%d\t%d\t%d\t%d" %
+              (board[i][0], board[i][1], board[i][2], board[i][3], board[i][4]))
 
 
 def main2(first='X'):
     board = board_initial()
-    while True:
+    step = 0
+    while step < 100:
+        step += 1
         p1 = remainingTroop(board, 1)
         p2 = remainingTroop(board, -1)
         if p1 == 16:
@@ -165,15 +220,19 @@ def main2(first='X'):
             if temp == None:
                 return 1
             else:
-                updateBoard(board, temp[0], temp[1])
+                print(first, temp)
+                board = updateBoard(board, temp[0], temp[1])
             first = 'O'
         elif first == 'O':
             temp = move(board, 1)
             if temp == None:
                 return -1
             else:
-                updateBoard(board, temp[0], temp[1])
+                print(first, temp)
+                board = updateBoard(board, temp[0], temp[1])
             first = 'X'
+        printBoard(board)
+        print("=======")
 
 
 def main():
@@ -184,7 +243,11 @@ def main():
     # board = board_initial()
     # a = board_clone(board)
     # print(a)
-    print(main2())
+    win = main2()
+    if win == 1:
+        print("O: win")
+    else:
+        print("X: win")
 
 
 if __name__ == '__main__':
