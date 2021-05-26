fbf80842d522778511e7a3ddf1dc34f52c94caf1
Sasuke99 <linh.huynh.dev.99@gmail.com>
2021-05-26 10:04:18 +0700

    init function

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import time
 import copy
 import random
+
+def board_initial():
+    board = [
+        [ 1,  1,  1,  1,  1],
+        [ 1,  0,  0,  0,  1],
+        [ 1,  0,  0,  0, -1],
+        [-1,  0,  0,  0, -1],
+        [-1, -1, -1, -1, -1]
+    ]
+    return board
+
+def move(board, player):
+    pass
+
+def ganh(board, current):
+    return board
+
+def vay(board, current):
+    return board 
+
+def remainingTroop(player):
+    totalTroop = 0
+    return  totalTroop
+
+def main():
+    pass 
+
+if __name__ == '__main__':
+    main()
